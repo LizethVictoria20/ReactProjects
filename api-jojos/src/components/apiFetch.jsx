645fcb22ec6d93940803dc5f8f8962e5814c9649
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-08 15:45:09 -0500

    Renderizando componente

--- api-jojos/src/components/apiFetch.jsx
+++ api-jojos/src/components/apiFetch.jsx
@@ -1,8 +1,13 @@
 import { React } from "react";
-import  { Container } from "./Container.jsx";
+import { useState } from 'react';
 
-function RenderAPI() {
-
+export function RenderAPI(url) {
+    const [ character, setCharacter] = useState([]);
+    fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+        setCharacter(res);
+    }, []);
+    return { character, RenderAPI };
 }
 
-export default RenderAPI;

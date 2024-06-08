9c90159da25024c75e9da6d30ec7a8affe85bc61
LizethVictoria20 <lizvictoria754@gmail.com>
2024-06-08 17:03:27 -0500

    Renderizando img

--- api-jojos/src/components/Cards.jsx
+++ api-jojos/src/components/Cards.jsx
@@ -3,13 +3,15 @@ import '../styles/cards.css';
 
 export function Cards(){
     const { character } = RenderAPI('https://stand-by-me.herokuapp.com/api/v1/characters');
-  
+    const urlImg = 'https://jojos-bizarre-api.netlify.app/assets/'
     return (
       <div className='container'>
 
         {character?.map((data) => 
         <div key={data.id} className='card'>
-            <p className='character-name'>{data.name}</p>
+            <h3 className='character-name'>{data.name}</h3>
+            <p>{data.japaneseName}</p>
+            <img src={urlImg + data.image} alt='Imagen personaje'/>
         </div>)}
       </div>
   

79f381fb6c151979054c23fcc3c0a515dd9c0509
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-01 16:09:15 -0500

    Haciendo llamado a la api

--- jojo-api/src/components/RenderAPI.jsx
+++ jojo-api/src/components/RenderAPI.jsx
@@ -1,22 +1,21 @@
 import { useState, useEffect } from "react";
+// import Card from "./Card";
 
-function RenderAPI(url) {
+function RenderAPI() {
   const [characters, setCharacters] = useState([]);
-  const fetchApi = () => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        setCharacters(res);
+
+  useEffect(() => {
+    fetch("https://stand-by-me.herokuapp.com/api/v1/characters")
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data);
       })
       .catch((error) => {
         console.error(`Hubo un error ${error}`);
       });
-  };
+  }, []);
 
-  useEffect(() => {
-    fetchApi();
-  });
-  return { characters, RenderAPI };
+  return <>{console.log(characters)}</>;
 }
 
 export default RenderAPI;

61c971d26f6b5c5b4347fd0883bb96fd162253c0
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-08 11:17:20 -0500

    Agregando logo en pagina de detalle

--- jojo-api/src/App.js
+++ jojo-api/src/App.js
@@ -1,13 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import DataAPI from "./components/DataAPI";
 import Detail from "./components/Details/Detail";
+import Logo from "./assets/JojosLogo.png";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Link to="/">
+        <div className="main-logo">
+          <img src={Logo} alt="Logo Jojo's" />
+        </div>
+      </Link>
       <Routes>
-        <Route exact path="/" element={<DataAPI/>} />
-        <Route path="/:characterID" element={<Detail/>} />
+        <Route exact path="/" element={<DataAPI />} />
+        <Route path="/:characterID" element={<Detail />} />
       </Routes>
     </Router>
   );
--- jojo-api/src/components/Card/Card.jsx
+++ jojo-api/src/components/Card/Card.jsx
@@ -1,5 +1,4 @@
 import "./style-card.css";
-import Logo from '../../assets/JojosLogo.png'
 import { Link } from "react-router-dom";
 
 function Card({ characters }) {
@@ -8,22 +7,19 @@ function Card({ characters }) {
   if (characters.length > 0) {
     return (
       <>
-      <div  className="main-logo">
-        <img src={Logo} alt="Logo Jojo's" />
-      </div>
-      <div className="container">
-        {characters.map((character) => {
-          return (
-            <Link to={character.id} key={character.id}>
-              <div className="card">
-                <h1 className="character-name">{character.name}</h1>
-                <p>{character.japaneseName}</p>
-                <img src={`${urlImg}${character.image}`} alt="" />
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+        <div className="container">
+          {characters.map((character) => {
+            return (
+              <Link to={character.id} key={character.id}>
+                <div className="card">
+                  <h1 className="character-name">{character.name}</h1>
+                  <p>{character.japaneseName}</p>
+                  <img src={`${urlImg}${character.image}`} alt="" />
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </>
     );
   }

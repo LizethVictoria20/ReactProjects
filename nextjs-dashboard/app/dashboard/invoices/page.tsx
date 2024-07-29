51ef0a6967096b4e9cb40638f7a849e325b5f77d
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-29 16:54:20 -0500

    Usando debounce para hacer la busqueda más optima

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,9 +1,10 @@
-import Table from "@/app/ui/customers/table";
-import { CreateInvoice } from "@/app/ui/invoices/buttons";
-import Search from "@/app/ui/search";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
-
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+ 
 export default async function Page({
   searchParams,
 }: {
@@ -14,11 +15,11 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-
+ 
   return (
-    <div className="w-fill">
+    <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl">Invoices</h1>
+        <h1 className='text-2xl'>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
@@ -27,6 +28,9 @@ export default async function Page({
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
     </div>
-  )
+  );
 }

a21ca5e13501b898ecef10bd38336baa1defb4d6
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-29 09:50:14 -0500

    renderizando componentes

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <p>Dashboard Page</p>
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+
+export default async function Page() {
+    const revenue = await fetchRevenue()
+    const latesInvoices = await fetchLatestInvoices()
+    console.log(revenue);
+
+    return (
+        <main>
+            <h1 className="mb-4 text-xl md:text-2xl">Dashboard</h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <RevenueChart revenue={revenue}/>
+                <LatestInvoices latestInvoices={latesInvoices}/>
+            </div>
+
+        </main>
+    ) 
 }
